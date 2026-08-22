b57eeae3ff35e4bae4968f563b1075bed62960be
ashikkhankuet <85025373+ashikkhankuet@users.noreply.github.com>
2026-08-22 15:51:36 +0600

    Add files via upload
    Deploy

--- nav.js
+++ nav.js
@@ -611,6 +611,7 @@ function renderFooter() {
         <div><h4>Company</h4><ul>
           <li><a href="/about">About</a></li>
           <li><a href="/faq">FAQ</a></li>
+          <li><a href="/contact">Contact</a></li>
           <li><a href="/privacy">Privacy Policy</a></li>
           <li><a href="/terms">Terms of Service</a></li>
         </ul></div>
